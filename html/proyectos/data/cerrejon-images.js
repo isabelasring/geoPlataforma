641dc2f56aa9d19b6918e37e944855e337e5ffd6
Geotrends SAS <geotrends@Mac-mini-de-Geotrends-2.local>
2026-02-19 18:51:57 -0500

    ajustes

--- html/proyectos/data/cerrejon-images.js
+++ html/proyectos/data/cerrejon-images.js
@@ -1,3 +1,3 @@
 var CERREJON_IMAGES = [
-  "1_web_opt.webp", "2_web_opt.webp", "3_web_opt.webp", "4_web_opt.webp", "5_web_opt.webp", "6_web_opt.webp"
+  "1.webp", "2_web_opt.webp", "3_web_opt.webp", "4_web_opt.webp", "5_web_opt.webp", "6_web_opt.webp"
 ];
--- html/proyectos/data/colcafe-images.js
+++ html/proyectos/data/colcafe-images.js
@@ -1,3 +1,3 @@
 var COLCAFE_IMAGES = [
-  "1_web_light.webp", "2_web_light.webp", "3_web_light.webp", "4_web_light.webp", "5_web_light.webp", "6_web_light.webp"
+  "1_web_light.webp", "2.webp", "3_web_light.webp", "4.webp", "5_web_light.webp", "6_web_light.webp"
 ];
